909125dd471f74953cc5db8e34227370d047bc3f
peter.biro <peter.biro@oliasoft.com>
2023-02-05 18:03:43 +0100

    created text component

---
+++ app/components/AppText.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Text, StyleSheet, Platform } from 'react-native';
+
+function AppText({children}) {
+    return (
+        <Text>
+            
+        </Text>
+    );
+}
+
+const styles = StyleSheet.create({
+    text: {
+        color: 'tomato',
+        ...Platform.select({
+            ios: {
+                fontSize: 18,
+                fontFamily: 'Avenir'
+            },
+            android: {
+                fontSize: 20,
+                fontFamily: 'Roboto'
+            }
+        })
+    }
+})
+
+export default AppText;
