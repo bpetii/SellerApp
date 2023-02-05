a68e566448bdda461fc84c76c87bbb90954d4019
peter.biro <peter.biro@oliasoft.com>
2023-02-05 17:44:56 +0100

    Created Button component

--- App.js
+++ App.js
@@ -2,10 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import AppButton from './app/components/AppButton';
 
 export default function App() {
+
   return (
-    <ViewImageScreen />
+    <View style={{
+      flex:1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <AppButton title='Login!' onPress={()=> console.log('clicked')}/>
+    </View>
   );
 }
 
