2649dc43601c4be0103badb724fdf9149b749dc0
peter.biro <peter.biro@oliasoft.com>
2023-02-09 20:27:59 +0100

    fix CategoryPickeItem onPress

--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { TouchableOpacity, StyleSheet } from 'react-native';
 import AppText from './AppText';
 import Icon from './Icon';
 
 function CategoryPickerItem({item, onPress}) {
-    return <View style={styles.container}>
-        <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80}/>
-        <AppText style={styles.label}>{item.label}</AppText>
-    </View>;
+    return (
+        <TouchableOpacity style={styles.container} onPress={onPress}>
+            <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80}/>
+            <AppText style={styles.label}>{item.label}</AppText>
+        </TouchableOpacity>
+    );
 }
 const styles = StyleSheet.create({
     container: {
