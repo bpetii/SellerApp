20278d73df3d75f57d612e44cc224676a8ed1430
peter.biro <peter.biro@oliasoft.com>
2023-02-12 15:07:37 +0100

    use eslint on ListingsScreen.js

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
 import Card from '../components/Card';
-import Screen from '../components/Screen'
+import Screen from '../components/Screen';
 import colors from '../config/colors';
 import routes from '../navigation/routes';
 import listingsApi from '../api/listings';
@@ -10,42 +10,45 @@ import AppButton from '../components/AppButton';
 import ActivityIndicator from '../components/ActivityIndicator';
 import useApi from '../hooks/useApi';
 
-function ListingsScreen({navigation}) {
-    const {data: listings, error, loading, request: loadListings } = useApi(listingsApi.getListings);
-    
-    useEffect(()=> {
-        loadListings();
-    },[]);
+function ListingsScreen({ navigation }) {
+  const {
+    data: listings, error, loading, request: loadListings,
+  } = useApi(listingsApi.getListings);
 
-    return (
-        <Screen style={styles.screen}>
-            {error && <>
-                <AppText>Could not retrieve the listings.</AppText>
-                <AppButton title="Retry" onPress={loadListings}/>
-            </>
-            }
-            <ActivityIndicator visible={loading}/>
-            <FlatList 
-                data={listings}
-                keyExtractor={(listing) => listing.id.toString()}
-                renderItem={({item}) =>
-                    <Card 
-                        title={item.title}
-                        subTitle={`$${item.price}`}
-                        imageUrl={item.images[0].url}
-                        onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
-                    />
-                }
-            />
-        </Screen>
-    );
+  useEffect(() => {
+    loadListings();
+  }, []);
+
+  return (
+    <Screen style={styles.screen}>
+      {error && (
+      <>
+        <AppText>Could not retrieve the listings.</AppText>
+        <AppButton title="Retry" onPress={loadListings} />
+      </>
+      )}
+      <ActivityIndicator visible={loading} />
+      <FlatList
+        data={listings}
+        keyExtractor={(listing) => listing.id.toString()}
+        renderItem={({ item }) => (
+          <Card
+            title={item.title}
+            subTitle={`$${item.price}`}
+            imageUrl={item.images[0].url}
+            onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
+          />
+        )}
+      />
+    </Screen>
+  );
 }
 
-const styles= StyleSheet.create({
-    screen: {
-        padding: 20,
-        backgroundColor: colors.light
-    }
-})
+const styles = StyleSheet.create({
+  screen: {
+    padding: 20,
+    backgroundColor: colors.light,
+  },
+});
 
 export default ListingsScreen;
