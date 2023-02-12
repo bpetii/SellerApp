89a9174b9dfafb149076a4e3f00c4a206c9b516f
peter.biro <peter.biro@oliasoft.com>
2023-02-12 15:58:02 +0100

    apply eslint on AppformField

--- app/components/forms/AppFormField.js
+++ app/components/forms/AppFormField.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import { useFormikContext } from 'formik';
 
 import AppTextInput from '../AppTextInput';
 import ErrorMessage from './ErrorMessage';
 
-function AppFormField({name, width = "100%", ...otherProps}) {
-    const {setFieldTouched, handleChange, errors, touched} = useFormikContext();
-    return (
-        <>
-            <AppTextInput
-                onChangeText={handleChange(name)}
-                onBlur={() => setFieldTouched(name)}
-                width={width}
-                {...otherProps}
-            />
-            <ErrorMessage visible={touched[name]} error={errors[name]}/>
-        </>
-    );
+function AppFormField({ name, width = '100%', ...otherProps }) {
+  const {
+    setFieldTouched, handleChange, errors, touched,
+  } = useFormikContext();
+  return (
+    <>
+      <AppTextInput
+        onChangeText={handleChange(name)}
+        onBlur={() => setFieldTouched(name)}
+        width={width}
+        {...otherProps}
+      />
+      <ErrorMessage visible={touched[name]} error={errors[name]} />
+    </>
+  );
 }
-
-const styles = StyleSheet.create({
-    
-})
 
 export default AppFormField;
